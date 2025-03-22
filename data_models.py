ee62b53354062ab5d63b0c1fb1d9c60d4d88503a
kehalit <kahineb.kn@gmail.com>
2025-03-22 21:57:40 +0100

    for more code readability docstring added

--- data_models.py
+++ data_models.py
@@ -5,34 +5,67 @@ from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
+
 class Author(db.Model):
-    __tablename__= 'authors'
+    """
+        Represents an author in the database.
+
+        Attributes:
+            author_id (int): The unique identifier for the author.
+            name (str): The name of the author.
+            birth_date (datetime): The birth date of the author.
+            date_of_death (datetime): The death date of the author (if applicable).
+            books (list): A list of books associated with the author.
+
+        Methods:
+            __repr__(): Returns a string representation of the Author object.
+    """
+    __tablename__ = 'authors'
+
     author_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), nullable=False)
-    birth_date =db.Column(db.DateTime)
+    birth_date = db.Column(db.DateTime)
     date_of_death = db.Column(db.DateTime)
 
     books = db.relationship('Book', backref='author', lazy=True)
 
     def __repr__(self):
-        return (f"Author(id={self.author_id}, name={self.name}, "
-                f"birth_date={self.birth_date}, date_of_death={self.date_of_death})")
+
+        return (
+                f"Author(id={self.author_id}, name={self.name}, "
+                f"birth_date={self.birth_date}, date_of_death={self.date_of_death})"
+            )
 
 
 class Book(db.Model):
-    __tablename__= 'books'
+    """
+        Represents a book in the database.
+
+        Attributes:
+            book_id (int): The unique identifier for the book.
+            isbn (str): The ISBN number of the book.
+            title (str): The title of the book.
+            publication_year (int): The year the book was published.
+            author_id (int): The unique identifier for the author of the book.
+
+        Methods:
+            __repr__(): Returns a string representation of the Book object.
+        """
+    __tablename__ = 'books'
+
     book_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     isbn = db.Column(db.String, nullable = False)
-    title =db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
     publication_year = db.Column(db.Integer)
 
-    author_id =db.Column(db.Integer, db.ForeignKey('authors.author_id'), nullable=False)
-
-
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'), nullable=False)
 
     def __repr__(self):
-        return (f"Book(id={self.book_id}, isbn={self.isbn}, "
-                f"title={self.title}, publication_year={self.publication_year}, author_id={self.author_id})")
-
-
-
+        """
+               Returns a string representation of the Book object.
+        """
+        return (
+            f"Book(id={self.book_id}, isbn={self.isbn}, "
+            f"title={self.title}, publication_year={self.publication_year}, "
+            f"author_id={self.author_id})"
+        )
